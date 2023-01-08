b153a35da7a3da3bb80592c673cb60f52c3ca300
tanmaykumarbala <tanmaykumarbala@gmail.com>
2023-01-08 13:42:55 +0530

    Refined the script

--- enableWifi.sh
+++ enableWifi.sh
@@ -1,7 +1,20 @@
-$(rm -rf /home/tanmaybala/Downloads/rtl8852be)
-$(cd /home/tanmaybala/Downloads)
+homePath="/home/tanmaybala/Downloads/"
+repoPath=$homePath"rtl8852be"
+
+echo "Installing Dependencies."
+$(sudo dnf install kernel-headers kernel-devel-`uname -r` make automake cmake gcc gcc-c++ bc)
+sleep 1
+echo "Removing old Directory if exist."
+if [ -d $repoPath ] then
+	$(rm -rf $repoPath)
+fi
+$(cd $homePath)
+echo "Cloning Repo."
 $(git clone https://github.com/HRex39/rtl8852be.git -b dev)
-$(cd /home/tanmaybala/Downloads/rtl8852be)
+echo "Entering in repo direcory."
+$(cd $repoPath)
+echo "Compiling the driver frome repo."
 $(make -j8)
+echo "Installing the driver and enabling the wifi driver for rtl8852be"
 $(sudo make install)
 $(sudo modprobe 8852be)
